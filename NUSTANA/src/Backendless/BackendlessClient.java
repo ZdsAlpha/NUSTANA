e86b28fe080a9fcdf4cdbbbc98c1fa74b88adf48
ZDS Alpha <abzsoft999@hotmail.com>
2018-05-02 03:03:19 +0500

    Added BackendlessClient

---
+++ NUSTANA/src/Backendless/BackendlessClient.java
@@ -0,0 +1,81 @@
+package backendless;
+
+import org.json.*;
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author Abdul Rahman
+ */
+public class BackendlessClient {
+    public static final String server = "https://api.backendless.com/";
+    public static final String version = "v4.0";
+    private String applicationId;
+    private String secretKey;
+    public BackendlessClient(String applicationId,String secretKey) {
+        this.applicationId = applicationId;
+        this.secretKey = secretKey;
+    }
+    public HttpURLConnection CreateConnection(String method,String parameters,byte[] content,String contentType) throws IOException{
+        URL url = new URL(server+applicationId+"/"+secretKey+"/"+parameters);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(method);
+        connection.setRequestProperty("Content-Type", contentType);
+        if(content != null){
+            connection.setDoOutput(true);
+            DataOutputStream stream = new DataOutputStream(connection.getOutputStream());
+            stream.write(content);
+            stream.flush();
+            stream.close();
+        }
+        return connection;
+    }
+    public HttpURLConnection CreateConnection(String method,String parameters,byte[] data) throws IOException{
+        return CreateConnection(method, parameters,data,"application/json");
+    }
+    public HttpURLConnection CreateConnection(String method,String parameters) throws IOException{
+        return CreateConnection(method, parameters,null);
+    }
+    public String CreateRequest(String method,String parameters,byte[] content,String contentType) throws IOException{
+        HttpURLConnection connection = CreateConnection(method, parameters, content, contentType);
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+        int status = connection.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer output = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            output.append(inputLine);
+        }
+        in.close();
+        connection.disconnect();
+        return output.toString();
+    }
+    public String CreateRequest(String method,String parameters,byte[] content) throws IOException{
+        return CreateRequest(method,parameters,content,"application/json");
+    }
+    public String CreateRequest(String method,String parameters) throws IOException{
+        return CreateRequest(method,parameters,null);
+    }
+    public JSONObject CreateObject(String table,JSONObject object)throws IOException {
+        return new JSONObject(CreateRequest("POST", "data/"+table, object.toString().getBytes()));
+    }
+    public JSONObject UpdateObject(String table,JSONObject object,String objectId) throws IOException {
+        return new JSONObject(CreateRequest("PUT", "data/"+table+"/"+objectId, object.toString().getBytes()));
+    }
+    public JSONObject GetObject(String table,String objectId) throws IOException{
+        return new JSONObject(CreateRequest("GET","data/"+table+"/"+objectId));
+    }
+    public JSONObject DeleteObject(String table,String objectId) throws IOException{
+        return new JSONObject(CreateRequest("DELETE","data/"+table+"/"+objectId));
+    }
+    public JSONArray GetObjects(String table) throws IOException{
+        return new JSONArray(CreateRequest("GET","data/"+table));
+    }
+    public JSONArray GetObjects(String table,int offset) throws IOException{
+        return new JSONArray(CreateRequest("GET","data/"+table+"?offset"+offset));
+    }
+    public JSONArray GetObjects(String table,int offset,int limit)throws IOException{
+        return new JSONArray(CreateRequest("GET","data/"+table+"?offset"+offset+",pageSize="+limit));
+    }
+}
