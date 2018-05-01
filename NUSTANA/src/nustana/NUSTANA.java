d1e1a76896eb48dae782d2dd5610d3ca28ed5436
ZDS Alpha <abzsoft999@hotmail.com>
2018-05-02 02:57:02 +0500

    Added UI Theme

--- NUSTANA/src/nustana/NUSTANA.java
+++ NUSTANA/src/nustana/NUSTANA.java
@@ -5,13 +5,43 @@
  */
 package nustana;
 
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
 /**
  *
  * @author Abdul Rahman
  */
 public class NUSTANA {
+    //Application entry point
     public static void main(String[] args) {
-        
+        initializeUI();
+        JOptionPane.showMessageDialog(null, "Welcome to NUSTANA app.","NUSTANA",JOptionPane.INFORMATION_MESSAGE);
     }
-    
+    //Changes UI Theme
+    public static void initializeUI(){
+        UIManager.put( "control", new Color( 128, 128, 128) );
+        UIManager.put( "info", new Color(128,128,128) );
+        UIManager.put( "nimbusBase", new Color( 18, 30, 49) );
+        UIManager.put( "nimbusAlertYellow", new Color( 248, 187, 0) );
+        UIManager.put( "nimbusDisabledText", new Color( 128, 128, 128) );
+        UIManager.put( "nimbusFocus", new Color(115,164,209) );
+        UIManager.put( "nimbusGreen", new Color(176,179,50) );
+        UIManager.put( "nimbusInfoBlue", new Color( 66, 139, 221) );
+        UIManager.put( "nimbusLightBackground", new Color( 18, 30, 49) );
+        UIManager.put( "nimbusOrange", new Color(191,98,4) );
+        UIManager.put( "nimbusRed", new Color(169,46,34) );
+        UIManager.put( "nimbusSelectedText", new Color( 255, 255, 255) );
+        UIManager.put( "nimbusSelectionBackground", new Color( 104, 93, 156) );
+        UIManager.put( "text", new Color( 230, 230, 230) );
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex){}
+    }
 }
