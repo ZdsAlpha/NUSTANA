a5b2d7e02cb912acd0c8334978c3762a46f8f70a
saifu <saifu@192.168.1.4>
2018-05-10 21:49:29 +0500

    added password and user id fields and UpdateUser method

--- NUSTANA/src/nustana/User.java
+++ NUSTANA/src/nustana/User.java
@@ -8,6 +8,8 @@ package nustana;
 import backendless.BackendlessException;
 import java.io.IOException;
 import org.json.JSONObject;
+import tools.ExceptionHandling;
+import tools.UI;
 
 /**
  *
@@ -20,6 +22,7 @@ public class User {
     private static String phoneNumber;
     private static String userStatus;
     private static String password;
+    private static String userID;
     public static JSONObject Register(String name,String email,String phoneNumber,String password) throws IOException , BackendlessException{
         JSONObject properties = new JSONObject();
         properties.put("name", name);
@@ -34,7 +37,28 @@ public class User {
         phoneNumber = properties.getString("phoneNumber");
         userStatus = properties.getString("userStatus");
         password = properties.getString("password");
+        userID=properties.getString("objectId");
         return properties;
+    }
+    
+    public static JSONObject UpdateUser(String userName, String userEmail, String userPhoneNumber,  String userPassword )throws IOException,BackendlessException{
+         if (name==userName && phoneNumber==userPhoneNumber && password== userPassword && email==userEmail){
+             UI.InfoMsg("Please enter new information", "Account Registration!");
+             return null;
+        }
+         else{
+        JSONObject properties = new JSONObject();
+        properties.put("name", userName);
+        properties.put("phoneNumber",  userPhoneNumber);
+       
+      
+        JSONObject obj =NUSTANA.getClient().UpdateUser(userID, userEmail, userPassword, properties);
+        name = properties.getString("name");
+        email = properties.getString("email");
+        phoneNumber = properties.getString("phoneNumber");
+        password = properties.getString("password");
+        return obj;
+    }
     }
     public static JSONObject Reset(String email) throws IOException , BackendlessException{
         return NUSTANA.getClient().ResetPassword(email);
